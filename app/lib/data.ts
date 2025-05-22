167845a08c9dfb22bd178ab089f932f880d0199a
hiro0908 <hiro54924@gmail.com>
2025-05-22 20:11:21 +0900

    p7

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -10,6 +10,14 @@ import {
 import { formatCurrency } from './utils';
 
 const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
+const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
+const data = await sql<LatestInvoiceRaw[]>`
+  SELECT invoices.amount, customers.name, customers.image_url, customers.email
+  FROM invoices
+  JOIN customers ON invoices.customer_id = customers.id
+  ORDER BY invoices.date DESC
+  LIMIT 5`;
 
 export async function fetchRevenue() {
   try {
